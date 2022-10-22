be945a8e70e77f1f725c36c75b36186030bf922e
Pruxon <jasiekpr@gmail.com>
2022-10-22 12:35:53 +0200

    test

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
 from strategy.test import Test
+from strategy.testClass import Test2
 
 
 def main(test_str: str) -> None:
     card = Test()
     print(card.hello() + ' ' + test_str)
+    card2 = Test2()
+    
 
 
 if __name__ == "__main__":
