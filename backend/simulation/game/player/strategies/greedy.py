012ef3d8cbdde7ebd23e97ca71508f030c419a95
tomsloj <tomsloj@gmail.com>
2022-11-18 14:42:28 +0100

    remove simulation in greedy

--- backend/simulation/game/player/strategies/greedy.py
+++ backend/simulation/game/player/strategies/greedy.py
@@ -43,103 +43,60 @@ class GreedyStrategy(Strategy):
         my_deck: Deck = game_state.players_states[self.id].deck
         other_players: list[PlayerState] = [x for i, x in enumerate(game_state.players_states) if i!=self.id]
 
+        if len(other_players) > 1:
+            raise NotImplementedError
+
         if all(len(my_deck.cards) >= len(other_player.deck.cards) for other_player in other_players):
             random.shuffle(all_cards_to_collect)
             return all_cards_to_collect
         else:
-            # print("select_best_permutation")
-            print(all_cards_to_collect)
             return self.select_best_permutation(game_state.players_states[self.id], other_players, all_cards_to_collect)
         # other_cards = [card for id in player_ids for card in to_collect[id] if id != self.id]
 
     def select_best_permutation(self, own_state: PlayerState, other_players: list[PlayerState], cards_to_collect: list[Card]) -> list[Card]:
-        best_cards_ammounts = -1
+        best_cards_amounts = -1
         best_order: list[Card] = []
-
-        max_deck: int = 0
 
         other_players_copy1 = copy.deepcopy(other_players)
         own_state_copy1 = copy.deepcopy(own_state)
-        for player in other_players_copy1:
-            player.deck.pop_n_top(len(own_state_copy1.deck.cards))
-            max_deck = max(max_deck, len(player.deck.cards))
-        length_of_permutation: int  = min(len(cards_to_collect), max_deck)
 
-        own_state_copy1.strategy = StrategyType.RANDOM_CARDS
-        own_state_copy1.deck.pop_all()
-        print(length_of_permutation)
+        
+
+        length_of_permutation: int  = min(len(other_players[0].deck.cards[len(own_state.deck.cards):]), len(cards_to_collect), 8)
+
         for to_collect in permutations(cards_to_collect, length_of_permutation):
+            other_deck = other_players[0].deck.cards[len(own_state.deck.cards):]
+            own_deck = cards_to_collect
+            # assert len(other_deck) == len(other_players_copy1[0].deck.cards[len(own_state.deck.cards):])
+            # assert len(own_deck) == len(cards_to_collect)
             
-            # other_players_copy = copy.deepcopy(other_players)
-            # own_state_copy = copy.deepcopy(own_state)
-            # # print("\nnext permutation")
-            # for player in other_players_copy:
-            #     # print(len(other_players_copy), "orginal player deck", to_collect, player.deck.cards)
-            #     player.deck.pop_n_top(len(own_state_copy.deck.cards))
-            #     # print(len(other_players_copy), "new player deck", to_collect, player.deck.cards)
-            # # print("orginal own deck", own_state_copy.deck.cards)
-            # own_state_copy.deck.pop_all()
-            own_state_copy = copy.deepcopy(own_state_copy1)
-            own_state_copy.deck.push_bottom(list(to_collect))
-            # print("new own deck", own_state_copy.deck.cards)
+            can_collect: int = 0
+            buffer_size: int = 1
+            visible_card: bool = True
+            
+            # check how many cards we can take with this permutation 
+            for i, x in enumerate(to_collect):
+                if not visible_card:
+                    buffer_size += 1
+                    visible_card = True
+                    continue
+                if x == other_deck[i]:
+                    buffer_size += 1
+                    visible_card = False
+                elif x > other_deck[i]:
+                    can_collect += buffer_size
+                    buffer_size = 1
+                else:
+                    buffer_size = 1
 
-            other_players_copy = copy.deepcopy(other_players_copy1)
-            all_players = other_players_copy.copy()
-            all_players.insert(0, own_state_copy)
-            # other_players.insert(0, own_state)
-            playersConfig = [PlayerConfig(x.strategy, [str(y) for y in x.deck.cards]) for x in other_players_copy]
-            playersConfig.insert(0, PlayerConfig(own_state_copy.strategy, [str(x) for x in to_collect]))
-            config = GameConfig(
-                                players=
-                                    # PlayerConfig(own_state.strategy, [str(x) for x in cards_to_collect]),
-                                    playersConfig
-                                ,
-                                cards_distribution=CardsDistribution.FIXED,
-                                max_turns=len(to_collect)
-                            )
-            # game = Game(config, debug=False)
-            # game.play()
-            game_state = GameState(all_players)
-            game_state.to_collect_by_id[0]
-            simulator = SimulatorV2(config, game_state)
-
-            player_strategies: dict[int, Strategy] = {}
-            for player_state in game_state.players_states:
-                strategy = RandomPlayersStrategy
-                player_strategies[player_state.id] = strategy(player_state.id, config)
-            # print("strategies", player_strategies)
-
-
-            for _ in range(len(cards_to_collect) + 1):
-                collected_cards: list[Card] = []
-                # print("game_state.to_collect_by_id[0]", game_state.to_collect_by_id[0])
-                if game_state.to_collect_by_id[0] is not None:
-                    collected_cards = player_strategies[
-                        game_state.to_collect_by_id[0]
-                    ].collect(game_state)
-                # print("collected_cards", collected_cards)
-                game_state = simulator.turn(collected_cards)
-
-
-            # print("own", game_state.players_states[0].deck.cards)
-            # print("enemy", game_state.players_states[1].deck.cards)
-            if len(game_state.players_states[0].deck.cards) > best_cards_ammounts:
-                best_cards_ammounts = len(game_state.players_states[0].deck.cards)
+            # print(can_collect, to_collect)
+            if can_collect > best_cards_amounts:
+                best_cards_amounts = can_collect
                 best_order = list(to_collect)
+            
+            if best_cards_amounts == length_of_permutation:
+                # we wont get better card layout
+                break
         best_order[len(best_order):] = set(cards_to_collect) - set(best_order)
         return best_order
 
-# def get_strategy(strategy_type: StrategyType) -> Type[Strategy]:
-#     strategy: Type[Strategy] | None = StrategiesMap.get(strategy_type)
-#     if strategy is None:
-#         raise ValueError(f"Strategy {strategy_type} not found")
-
-#     return strategy
-
-# StrategiesMap: dict[StrategyType, Type[Strategy]] = {
-#     # StrategyType.OWN_FIRST: OwnFirstStrategy,
-#     # StrategyType.OWN_LAST: OwnLastStrategy,
-#     StrategyType.RANDOM_CARDS: RandomCardsStrategy,
-#     StrategyType.RANDOM_PLAYERS: RandomPlayersStrategy,
-#     StrategyType.GREEDY: GreedyStrategy,
-# }
